f5a925bb09b0a5855624e1b650bb118701210b3c
Ahmad Raza Khan <khanahmad.ak686@gmail.com>
2026-07-12 14:51:56 +0530

    Add PasswordReset model

---
+++ server/models/PasswordReset.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const passwordResetSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    otp: {
+      type: String,
+      required: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model(
+  "PasswordReset",
+  passwordResetSchema
+);
